c6b98131b56d72f26a4f4c39e6db886dbc1dc47e
Lucas <luucas.chaves@gmail.com>
2022-05-16 13:19:07 -0300

    First Commit

---
+++ backend/src/main/java/com/lucaschaves/lcmovie/LcmovieApplication.java
@@ -0,0 +1,13 @@
+package com.lucaschaves.lcmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LcmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LcmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/lucaschaves/lcmovie/LcmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucaschaves.lcmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LcmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
